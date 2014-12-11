cd6f75d676cdedd7aef7dba78bf7b0380b9adc5e
MatTracy <MatTracy@10.0.1.2>
2014-12-10 20:32:07 -0500

    Added the following Header:  * PR2 (Personal Record Recorder) An application created to record personal  * records of CrossFit athletes Created by Luis Dall, Tracy Devault and Stephen  * Blackburn Group 1: CMSC 495 7981 Current Trends and Projects in Computer  * Science (2148)  * Instructor: Dr. Hung Dao AND made sure all the GUI components variables were named...

--- src/IllegalFormat.java
+++ src/IllegalFormat.java
@@ -1,11 +1,19 @@
-/**The IllegalFormat class creates an exception.
-@ author Luis Dall
-*/
 
-public class IllegalFormat extends NoSuchFieldException
-{
-	public IllegalFormat( String message )
-	{
-		super(message);
-	}
+/**
+ * PR2 (Personal Record Recorder) An application created to record personal
+ * records of CrossFit athletes Created by Luis Dall, Tracy Devault and Stephen
+ * Blackburn Group 1: CMSC 495 7981 Current Trends and Projects in Computer
+ * Science (2148) 
+ * Instructor: Dr. Hung Dao
+ *
+ * The IllegalFormat class creates an exception.
+ *
+ * @ author Luis Dall
+ */
+
+public class IllegalFormat extends NoSuchFieldException {
+
+    public IllegalFormat(String message) {
+        super(message);
+    }
 }
